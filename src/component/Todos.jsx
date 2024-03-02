043f2b11c4b2cf1bdea5fc932b82505dc7da0182
imran <zunaeede@gmail.com>
2024-03-02 09:39:36 +0600

    main - remove npm

---
+++ src/component/Todos.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Todo from './Todo';
+
+
+
+const Todos = (props) => {
+  return (
+    <section>
+      {props.todos.map((todo) => (
+        <Todo
+          todo={todo.todo}
+          key={todo.id}
+          id={todo.id}
+          onRemoveTodo={props.onRemoveTodo}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Todos;
